c033a9d4ef4e7d5268532ea47cde44dfd2c4c206
dineshvincent@macbook <dineshvincent@gmail.com>
2023-10-28 13:15:25 +0530

    Enhanced Palindrome App to use Mystack class for palindrome test also and added the result to the output html.

---
+++ src/test/java/com/dinesh/palindromeapp/service/TestPalindromeCheckerMyStackImpl.java
@@ -0,0 +1,42 @@
+package com.dinesh.palindromeapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestPalindromeCheckerMyStackImpl{
+
+    private final PalindromeChecker palindromeChecker;
+
+    TestPalindromeCheckerMyStackImpl(){
+        palindromeChecker = new PalindromeCheckerMystackImpl();
+    }
+    @Test
+    void checkForPalindrome() {
+
+        assertTrue(palindromeChecker.IsPalindrome("aaaaa"));
+        assertFalse(palindromeChecker.IsPalindrome("dinesh"));
+    }
+
+    @Test
+    void checkForPalindromeList(){
+        List<String> words = new ArrayList<>();
+        words.add("madam");
+        words.add("aaaaa");
+        words.add("bbb");
+        words.add("reviver");
+        words.add("Excitement");
+
+        Map<String, Boolean> mapTestResult =  palindromeChecker.TestForPalindrome(words);
+
+        assertTrue(mapTestResult.get("aaaaa"));
+        assertFalse(mapTestResult.get("Excitement"));
+        assertTrue(mapTestResult.get("bbb"));
+
+    }
+}
